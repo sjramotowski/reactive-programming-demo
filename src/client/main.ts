f8bd2d0b82e9045505a12b348fb0481e30f140f4
sjramotowski <sjramoski@gmail.com>
2024-11-04 20:59:47 -0500

    Update main.ts

--- src/client/main.ts
+++ src/client/main.ts
@@ -8,16 +8,14 @@ const form = document.getElementById("form")!;
 const submitEvents$ = fromEvent<FormDataEvent>(form, 'submit')
 
 const userMessages$ = submitEvents$.pipe(
-    // Prevent the default form submission behavior (page reload)
+
     tap((e) => e.preventDefault()),
-    // Get the message input from the form and clear it after reading the value
     map((e: FormDataEvent) => {
         const messageInput: HTMLInputElement = (e.currentTarget as HTMLFormElement).querySelector('input[name="message"]')!;
         const message = messageInput.value;
         messageInput.value = "";
         return message;
     }),
-    // Map the string message to a full Message object with a timestamp and action
     map((message: string): Message => {
         return {data: message, action: "sent", timestamp: new Date()};
     }),
